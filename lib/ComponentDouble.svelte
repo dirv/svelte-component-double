f4824c28335a83dcc8e652f438f5d37229718c49
Kim Schmider <hello@schmider.kim>
2020-10-09 23:20:15 +0200

    minor: remove now unnecessary line of code

--- lib/ComponentDouble.svelte
+++ lib/ComponentDouble.svelte
@@ -2,7 +2,6 @@
   const { _spyName: name, _spyInstance: instance, otherProps } = $$props;
 
   export function getNonSpyProps() {
-    const allProps = this.$$.ctx[3];
     return Object.keys($$props).reduce((acc, key) => {
       if (!key.startsWith("_spy")) {
         acc[key] = $$props[key];
